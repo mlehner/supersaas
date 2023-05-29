51f9f00ecc30ea4c58e73b142aa05991f62f3b82
Matt Lehner <matt.lehner@coachmeplus.com>
2023-05-29 15:36:49 -0400

    update encore and stimulus

--- assets/controllers/bookings_controller.js
+++ assets/controllers/bookings_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from 'stimulus';
+import { Controller } from '@hotwired/stimulus';
 import { formatRange } from '@fullcalendar/core';
 
 export default class extends Controller {
--- assets/controllers/calendar_controller.js
+++ assets/controllers/calendar_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from 'stimulus';
+import { Controller } from '@hotwired/stimulus';
 import { Calendar } from '@fullcalendar/core';
 import listPlugin from '@fullcalendar/list';
 
